0f46c71259fddd6ae523c3e7fe7fe6b4dc5d5330
marukaz <marukaz.jh@gmail.com>
2018-12-13 15:51:38 +0900

    new script

---
+++ create_dataset/process_json.py
@@ -0,0 +1,36 @@
+import argparse
+import json
+
+import numpy as np
+
+
+def main(args):
+    r"""
+    正解文がhyposの先頭にあるという前提のもと、正解文が一番長くなければ、hyposをユニークにし標準出力にjson形式で書き出す
+    :param args:
+    :return:
+    """
+    with open(args.load, 'r') as f:
+        for line in f:
+            unique_hypos = []
+            texts = []
+            try:
+                d = json.loads(line)
+            except json.JSONDecodeError:
+                continue
+            longest = np.argmax([len(hypo['text']) for hypo in d['hypos']])
+            if longest != 0:
+                for hypo in d['hypos']:
+                    text = hypo['text']
+                    if text not in texts:
+                        unique_hypos.append(hypo)
+                        texts.append(text)
+                d['hypos'] = unique_hypos
+                print(json.dumps(d, ensure_ascii=False))
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('load')
+    args = parser.parse_args()
+    main(args)
