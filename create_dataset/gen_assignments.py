08a3067b23c8a686c997c43a1238e786e40597f3
Kazuki Matsumaru <marukaz.jh@gmail.com>
2018-12-13 23:35:43 +0900

    fix for argparse

--- create_dataset/gen_assignments.py
+++ create_dataset/gen_assignments.py
@@ -3,15 +3,18 @@ import json
 import numpy as np
 
 
-def main():
+def main(args):
     assignments = np.load('/home/6/18M31289/entasum/create_dataset/generate_candidates/data/assignments-pretrained.npy')
-    with open('/home/6/18M31289/entasum/create_dataset/generate_candidates/data/data.json') as f:
+    with open(args.json_file) as f:
         for a, line in zip(assignments, f):
             d = json.loads(line)
             for ix in a:
                 print(d['hypos'][ix])
-            print('--------------------------------------------------')
+            print('------------------------------------------------------------')
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('json_file')
+    args = parser.parse_args()
+    main(args)
