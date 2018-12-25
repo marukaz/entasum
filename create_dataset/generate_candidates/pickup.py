4f42a05069443b8683b756e309870cc4819f9ec7
Kazuki Matsumaru <marukaz.jh@gmail.com>
2018-12-25 23:13:30 +0900

    shorter argument

--- create_dataset/generate_candidates/pickup.py
+++ create_dataset/generate_candidates/pickup.py
@@ -7,7 +7,7 @@ def main(args):
     with open(args.load, 'r') as f:
         unit_list = [unit[:-1] for unit in zip(*[iter(f)] * args.lines_per_unit)]
     for unit in random.sample(unit_list, args.sample_num):
-        print(*unit)
+        print(*unit, end='')
         print('************************************************************************************************')
 
 
@@ -15,6 +15,6 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('load')
     parser.add_argument('--lines-per-unit', type=int, default=7)
-    parser.add_argument('--sample-num', type=int, default=50)
+    parser.add_argument('-n', '--sample-num', type=int, default=50)
     args = parser.parse_args()
     main(args)
