146cf6ed1fad69594f1654e19dc7f78b987909d0
marukaz <marukaz.jh@gmail.com>
2019-01-21 16:20:35 +0900

    choice label and type

--- scripts/snli_style2pair.py
+++ scripts/snli_style2pair.py
@@ -9,13 +9,19 @@ def main(args):
     with open(args.snli_like_file, 'r') as rf, open(src_path, 'w') as sf, open(tgt_path, 'w') as tf:
         for line in rf:
             d = json.loads(line)
-            print(d['sentence1'], file=sf)
-            print(d['sentence2'], file=tf)
+            if ((args.label is None or d['gold_label'] == args.label)
+                    and (args.type is None or d['headline_type'] == args.type)):
+                print(d['sentence1'], file=sf)
+                print(d['sentence2'], file=tf)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('snli_like_file', type=str, help='path to snli format jsonl file')
     parser.add_argument('-o', '--output-prefix', type=str, help="output files name's prefix")
+    parser.add_argument('-l', '--label', choices=['entailment', 'contradiction'],
+                        help="output only the specified label")
+    parser.add_argument('-t', '--type', choices=['reference', 'best', 'none'],
+                        help="output only the specified type")
     args = parser.parse_args()
     main(args)
