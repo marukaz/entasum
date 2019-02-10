390d0c14b5bbc806adcb65ab1e3b160904f8bf6b
marukaz <marukaz.jh@gmail.com>
2019-02-10 16:42:31 +0900

    introduce pathlib

--- create_dataset/generate_candidates/target_classifier.py
+++ create_dataset/generate_candidates/target_classifier.py
@@ -1,6 +1,7 @@
 import argparse
 import json
 from operator import itemgetter
+from pathlib import Path
 import random
 
 import numpy as np
@@ -63,9 +64,11 @@ def main(args):
         elif args.format == 'yahoo':
             header = '設問ID(半角英数字20文字以内)\tチェック設問有無(0:無 1:有)\tチェック設問の解答(F04用)\t' \
                      'F01:ラベル\tF02:ラベル\tF03:ラベル\tF04:チェックボックス\tF05:ラベル'
-        with open(args.tsv_file_name, 'w') as wf, open(f'location_{args.tsv_file_name}', 'w') as idf:
+        file_path = Path(args.tsv_file_path)
+        loc_file_path = file_path.parent / ('location_' + file_path.name)
+        with file_path.open('w') as wf, loc_file_path.open('w') as locf:
             print(header, file=wf)
-            print('question_id\tref_id\tbest_id', file=idf)
+            print('question_id\tref_id\tbest_id', file=locf)
             for i, (snt_b, probas, src) in enumerate(zip(corpus_batch_itr, probas_batch_itr, sources)):
                 reference = snt_b[0]
                 best = snt_b[1]
@@ -96,7 +99,7 @@ def main(args):
                     joined_headlines = "@".join(headlines) + '@該当なし'
                     print(f'{i}\t0\t\t記事の内容から逸脱していない見出しを全てチェックしてください。\t記事\t{src}\t'
                           f'{joined_headlines}\t記事の内容から逸脱していない見出しを全てチェックしてください。', file=wf)
-                print(f'{i}\t{ref_loc}\t{best_loc}', file=idf)
+                print(f'{i}\t{ref_loc}\t{best_loc}', file=locf)
                 if args.verbose:
                     print(f'source: {src}')
                     print(f'ref loc: {ref_loc}, best loc: {best_loc}')
@@ -110,7 +113,7 @@ if __name__ == "__main__":
     parser.add_argument('ppl_file', nargs='?', default=None)
     parser.add_argument('-n', '--clf-name', default='model/clf.pkl')
     parser.add_argument('--format', choices=['default', 'yahoo'], default='default')
-    parser.add_argument('--tsv-file-name', default='data/yahoo_template81')
+    parser.add_argument('--tsv-file-path', default='data/out.tsv')
     parser.add_argument('--choice-num', type=int, default=6)
     parser.add_argument('-bow', '--bag-of-words', action='store_true')
     parser.add_argument('--verbose', action='store_true')
