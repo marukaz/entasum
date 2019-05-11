3379b7573c3d5fd13bc42c62ba5e41a3a28b42c3
marukaz <marukaz.jh@gmail.com>
2019-05-11 16:17:18 +0900

    update for tsv format

--- scripts/extract_entail_data.py
+++ scripts/extract_entail_data.py
@@ -6,22 +6,28 @@ from tqdm import tqdm
 
 
 def main(args):
-    with open(args.snli_like_file, 'r') as sf, open(args.prob_file, 'r') as pf:
+    prob_format = args.prob_file.split('.')[-1]
+    with open(args.data_file, 'r') as df, open(args.prob_file, 'r') as pf:
         if args.output_file is not None:
             output_file = open(args.output_file, 'w')
         else:
             output_file = sys.stdout
         prob_position = 1 if args.contradiction_first else 0
-        for data_d, prob_d in tqdm(zip(sf, pf)):
-            entailment_prob = json.loads(prob_d)['label_probs'][prob_position]
+        for data, prob in tqdm(zip(df, pf)):
+            if prob_format == 'tsv':
+                entailment_prob = prob.split('¥t')[prob_position]
+            elif prob_format == 'json' or prob_format == 'jsonl':
+                entailment_prob = json.loads(prob)['label_probs'][prob_position]
+            else:
+                raise ValueError(f'file format is not supported: {args.data_file}')
             if entailment_prob > 0.5:
-                output_file.write(data_d)
+                output_file.write(data)
         output_file.close()
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('snli_like_file', type=str, help='path to data file')
+    parser.add_argument('data_file', type=str, help='path to data file')
     parser.add_argument('prob_file', type=str, help='path to entailment probability file')
     parser.add_argument('-o', '--output-file', type=str, help='path to output file')
     parser.add_argument('-c', '--contradiction-first', action='store_true',
